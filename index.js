b2e7b238e7323e0a85008b48727a84a815097e45
pixprogrammer <145217871+PixProgrammer@users.noreply.github.com>
2023-12-22 10:45:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const TelegramBot = require('node-telegram-bot-api');
+import { Bot, webhookCallback } from "grammy";
+import express from "express";
 
-// Replace 'YOUR_BOT_TOKEN' with the actual token you get from the BotFather on Telegram
-const token = '6978212821:AAGVTBGjBZf6VlB8Esgqy7XfjWE5JIOTIhk';
-const port = process.env.PORT || 3000; // Port for the Express server
+// Create a bot using the Telegram token
+const bot = new Bot(process.env.TELEGRAM_TOKEN || "6978212821:AAGVTBGjBZf6VlB8Esgqy7XfjWE5JIOTIhk");
 
-// Create a new instance of the TelegramBot
-const bot = new TelegramBot(token);
-
-// Create an Express app
-const app = express();
-
-// Use body-parser middleware to parse incoming JSON requests
-app.use(bodyParser.json());
-
-// Handle the /start command
-bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id;
-    const username = msg.from.username;
-
-    bot.sendMessage(chatId, `Hi ${username}! Welcome to your bot.`);
+// Handle the /start command to greet the user
+bot.command("start", (ctx) => {
+  const username = ctx.from?.username || "there";
+  ctx.reply(`Hi ${username}! Welcome to your bot.`);
 });
 
-// Set up a route to handle incoming updates from Telegram
-app.post(`/bot${token}`, (req, res) => {
-    bot.processUpdate(req.body);
-    res.sendStatus(200);
-});
+// Start the server
+if (process.env.NODE_ENV === "production") {
+  // Use Webhooks for the production server
+  const app = express();
+  app.use(express.json());
+  app.use(webhookCallback(bot, "express"));
 
-// Start the Express server
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    
-    // Set up the webhook
-    const webhookUrl = 'https://frantic-cod-school-uniform.cyclic.app/bot' + token;
-    bot.setWebHook(webhookUrl);
-    console.log(`Webhook set up at ${webhookUrl}`);
-});
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Bot listening on port ${PORT}`);
+  });
+} else {
+  // Use Long Polling for development
+  bot.start();
+}
 
-// Log errors
-bot.on('webhook_error', (error) => {
-    console.error(error);
-});
-
-// Handle graceful shutdown
-process.once('SIGINT', () => {
-    bot.stopWebHook();
-    console.log('Bot stopped webhook');
-    process.exit();
-});
-
-process.once('SIGTERM', () => {
-    bot.stopWebHook();
-    console.log('Bot stopped webhook');
-    process.exit();
-});
