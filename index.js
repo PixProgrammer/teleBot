4a1156103109ef2c59b2d849f1dd8a2a49310b45
pixprogrammer <145217871+PixProgrammer@users.noreply.github.com>
2023-12-22 11:23:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-import { Bot, webhookCallback } from "grammy";
-import express from "express";
+const TelegramBot = require("node-telegram-bot-api");
+const express = require('express');
 
-// Create a bot using the Telegram token
-const bot = new Bot(process.env.TELEGRAM_TOKEN || "6978212821:AAGVTBGjBZf6VlB8Esgqy7XfjWE5JIOTIhk");
+const app = express();
 
-// Handle the /start command to greet the user
-bot.command("start", (ctx) => {
-  const username = ctx.from?.username || "there";
-  ctx.reply(`Hi ${username}! Welcome to your bot.`);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
 
-  const app = express();
-  app.use(express.json());
-  app.use(webhookCallback(bot, "express"));
+// replace the value below with the Telegram token you receive from @BotFather
+const token = "6978212821:AAGVTBGjBZf6VlB8Esgqy7XfjWE5JIOTIhk";
 
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Bot listening on port ${PORT}`);
-  });
+const bot = new TelegramBot(token, { polling: true });
 
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  const userInput = msg.text.toLowerCase();
+
+  // Check if the received message is "hi"
+  if (userInput === 'hi' || userInput === 'hello') {
+    // Respond with a greeting
+    bot.sendMessage(chatId, "Hello! I'm your bot. How can I help you?");
+  } else {
+    // Respond with a default message for other messages
+    bot.sendMessage(chatId, "I'm not programmed to respond to that. If you need help, just say 'hi'.");
+  }
+});
